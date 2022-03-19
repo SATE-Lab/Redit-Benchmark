8b9d7f2f9ec2a184b06155e3bc26c84c5291e55e
zhoumengbo <zhoumengbo1106@163.com>
2022-03-19 16:04:07 +0800

    init

--- MapReduce/src/main/java/main/MrSequential.java
+++ MapReduce/src/main/java/main/MrSequential.java
@@ -1,14 +1,17 @@
 package main;
 
 import mr.*;
-
+import mrapps.WordCount;
 import java.io.*;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+
+// simple sequential MapReduce.
+// args[] = [input_folderPath, output_filePath]
 
 public class MrSequential {
-
-    // for sorting by key.
-    private static HashMap<String,Integer> keyValue = new HashMap<>();
+    private static ArrayList<KeyValue> keyValues = new ArrayList<>();
+    private static ArrayList<KeyValue> intermediate = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         String folderPath = args[0];
@@ -18,13 +21,36 @@ public class MrSequential {
             FileReader fr = new FileReader(files[i]);
             BufferedReader br = new BufferedReader(fr);
             String line;
+            StringBuffer content = new StringBuffer();
             while((line = br.readLine()) != null){
-                //process the line
-                System.out.println(line);
+                content.append(line);
             }
-
+            keyValues = WordCount.map(content);
+            intermediate.addAll(keyValues);
         }
-    }
+        Collections.sort(intermediate);
 
+        File inputFile = new File(args[1]);
+        if(!inputFile.exists()){
+            inputFile.createNewFile();
+        }
+        FileWriter fileWriter = new FileWriter(inputFile,true);
+        int i = 0, j;
+        while (i < intermediate.size()){
+            j = i + 1;
+            while (j < intermediate.size() && intermediate.get(j).getKey().equals(intermediate.get(i).getKey())){
+                j++;
+            }
+            ArrayList<Integer> values = new ArrayList<>();
+            for (int k = i; k < j; k++){
+                values.add(intermediate.get(k).getValue());
+            }
+            int count = WordCount.reduce(values);
+            String data = intermediate.get(i).getKey() + " " + count + "\r\n";
+            fileWriter.write(data);
+            i = j;
+        }
+        fileWriter.close();
+    }
 
 }
---
+++ MapReduce/src/main/java/mr/KeyValue.java
@@ -0,0 +1,40 @@
+package mr;
+
+public class KeyValue implements Comparable<KeyValue>{
+    private String key;
+    private int value;
+
+    public KeyValue(String Key, int Value) {
+        this.key = Key;
+        this.value = Value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        key = key;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "KeyValue{" +
+                "Key='" + key + '\'' +
+                ", Value=" + value +
+                '}';
+    }
+
+    @Override
+    public int compareTo(KeyValue o) {
+        return this.key.compareTo(o.key);
+    }
+}
